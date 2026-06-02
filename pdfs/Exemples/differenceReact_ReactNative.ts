623478df4494c060dc5aba097774a4945f20363b
formateur357 <formateur357@gmail.com>
2026-06-02 09:55:52 +0200

    add exemple

---
+++ pdfs/Exemples/differenceReact_ReactNative.ts
@@ -0,0 +1,18 @@
+// Valide en react web mais, faux en react native
+
+return {
+    <div>
+        <p>Bonjour</p>
+        <button>Cliquer</button>
+    <div>
+};
+
+// Version react native
+return {
+    <View>
+        <Text>Bonjour</Text>
+        <Pressable>
+            <Text>Cliquer</Text>
+        </Pressable>
+    <View>
+};
